f0709c35183120c50643292c08edb994a3b7631b
jsimmons <jsimmons@triton-software.co.uk>
2019-07-29 11:46:50 +0100

    Energy rating and Warrantee banners on product listing

---
+++ app/code/Ninedotmedia/ProductAttributes/Block/Product/Energyrating.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Ninedotmedia\ProductAttributes\Block\Product;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\Registry;
+use Magento\Catalog\Model\Product;
+
+class Energyrating extends Template
+{
+    /**
+     * @var Product|null
+     */
+    private $product = null;
+
+    /**
+     * @var Registry
+     */
+    private $coreRegistry;
+
+    public function __construct(
+        Template\Context $context,
+        Registry $registry,
+        array $data = []
+    ) {
+        $this->coreRegistry = $registry;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @param Product $product
+     */
+    public function setProduct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * @return Product
+     */
+    public function getProduct()
+    {
+        if (!$this->product) {
+            $this->product = $this->coreRegistry->registry('product');
+        }
+        return $this->product;
+    }
+
+    /**
+     * @return array|null|string
+     */
+    public function getEnergyRatingText()
+    {
+        return ($product = $this->getProduct()) ?
+            $product->getAttributeText('energyrating') : null;
+    }
+}
---
+++ app/code/Ninedotmedia/ProductAttributes/Block/Product/Warrantee.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Ninedotmedia\ProductAttributes\Block\Product;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\Registry;
+use Magento\Catalog\Model\Product;
+use Magento\Swatches\Helper\Data as SwatchHelper;
+
+class Warrantee extends Template
+{
+    private $_swatchHelper;
+
+    /**
+     * @var Product|null
+     */
+    private $product = null;
+
+    /**
+     * @var Registry
+     */
+    private $coreRegistry;
+
+    public function __construct(
+        Template\Context $context,
+        SwatchHelper $swatchHelper,
+        Registry $registry,
+        array $data = []
+    ) {
+        $this->coreRegistry = $registry;
+        $this->_swatchHelper = $swatchHelper;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @param Product $product
+     */
+    public function setProduct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * @return Product
+     */
+    public function getProduct()
+    {
+        if (!$this->product) {
+            $this->product = $this->coreRegistry->registry('product');
+        }
+        return $this->product;
+    }
+
+    /**
+     * @return array|null|string
+     */
+    public function getWarranteeText()
+    {
+        return ($product = $this->getProduct()) ?
+            $product->getAttributeText('ndm_guarantee') : null;
+    }
+
+    /**
+     * @return array|null|string
+     */
+    public function getWarranteeImage()
+    {
+        $optionid = $this->getProduct()->getData('ndm_guarantee');
+        $swatchData = $this->_swatchHelper->getSwatchesByOptionsId([$optionid]);
+        return "/pub/media/attribute/swatch". $swatchData[$optionid]['value'];
+    }
+
+    /**
+     * @return array|null|string
+     */
+    public function hasWarranteeImage()
+    {
+        if ($optionid = $this->getProduct()->getData('ndm_guarantee')) {
+            $swatchData = $this->_swatchHelper->getSwatchesByOptionsId([$optionid]);
+            if (isset($swatchData[$optionid]) && !empty($swatchData[$optionid]['value'])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
