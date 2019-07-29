5c3226b3ed87e6af149e2db41a6d698e7a899463
jsimmons <jsimmons@triton-software.co.uk>
2019-07-29 14:12:48 +0100

    Added price low-high-low sort

---
+++ app/code/Ninedotmedia/Theme/Plugin/Catalog/Model/Config.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ninedotmedia\Theme\Plugin\Catalog\Model;
+
+class Config
+{
+    public function afterGetAttributeUsedForSortByArray(
+        \Magento\Catalog\Model\Config $catalogConfig,
+        $options
+    ) {
+
+        $options['low_to_high'] = __('Price - Low To High');
+        $options['high_to_low'] = __('Price - High To Low');
+        return $options;
+
+    }
+
+}
